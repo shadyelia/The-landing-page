3b545bc38e61f90f3cca2f3c71f66b5de135b7b8
shadyelia <shady.elia@hotmail.com>
2022-10-30 17:21:43 +0200

    adding some style

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # The-landing-page
 
-- Steps to run:
-    1- install node using: `npm install -g npm`
+- Steps to run: <br />
+    1- install npm cli using: `npm install -g npm`
     2- Install live-server using: `npm install live-server -g`
     3- Start the server using: `live-server`
+<br />
+<br />
+- You can add extra sections by adding `header` and `text` to data/data.json. <br />
--- js/app.js
+++ js/app.js
@@ -1,21 +1,37 @@
-import data from '../data/data.json' assert { type: "json" };
+import data from "../data/data.json" assert { type: "json" };
 
+const bindData = () => {
+  const navBarList = document.getElementById("nav-bar-list");
+  const sections = document.getElementById("sections-list");
 
-const navBarList = document.getElementById('nav-bar-list');
-const sections = document.getElementById('sections-list');
+  const virtualNavBar = document.createDocumentFragment();
+  const virtualSections = document.createDocumentFragment();
 
-const virtualNavBar = document.createDocumentFragment();
-const virtualSections = document.createDocumentFragment();
+  data.forEach((element) => {
+    addNavBarItem(navBarList, element);
+    addSectionItem(sections, element)
+  });
 
-data.forEach(element => {
-    const navBarItem = document.createElement('li');
-    navBarItem.innerHTML = element['header'];
-    navBarList.appendChild(navBarItem);
+  navBarList.children[0].className = 'active';
+  //navBarList = virtualNavBar;
+  //sections = virtualSections;
+};
 
-    const paragrahic = document.createElement('p');
-    paragrahic.innerText = element['text'];
+const addNavBarItem = (navBarList, element) => {
+  const navBarItem = document.createElement("li");
+  const navBarLink = document.createElement("a");
+  
+  navBarLink.innerHTML = element["header"];
+  navBarItem.appendChild(navBarLink);
+  navBarList.appendChild(navBarItem);
+};
+
+const addSectionItem = (sections, element) => {
+    const paragrahic = document.createElement("p");
+
+    paragrahic.innerText = element["text"];
+    paragrahic.className = 'card';
     sections.appendChild(paragrahic);
-});
+};
 
-//navBarList = virtualNavBar;
-//sections = virtualSections;
+bindData();
